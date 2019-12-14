98c7034f8d02bdf44c3bd77b8a134196b99c8a93
Dodo-1989 <naderzaeri89@gmail.com>
2019-12-14 10:46:59 -0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let homeBottun = document.querySelector(".home")
 let loginBottun = document.querySelector(".login")
 let body = document.querySelector("body")
@@ -5,6 +6,9 @@ let h1 = document.querySelector("h1")
 let prag = document.querySelector(".introduction")
 let image = document.querySelector("img")
 let imgBottun = document.querySelector(".change-photo")
+let dontTouch = document.querySelector(".dontTouch")
+let greetings = document.querySelector(".enterName")
+
 
 
 
@@ -30,6 +34,7 @@ loginBottun.addEventListener("mouseout", function(){
 
 loginBottun.onclick = function() {
 changeH1();
+h1.classList.remove("toggler")
 
 };
 
@@ -38,12 +43,41 @@ homeBottun.onclick = function(){
 
 }
 
+imgBottun.addEventListener("mouseover", function(){
+  this.classList.add("red")
+});
 
+imgBottun.addEventListener("mouseout", function(){
+  this.classList.remove("red")
+});
 
 imgBottun.onclick = function(){
     changedImage();
     image.classList.add("width")
+    
 
+}
+
+dontTouch.addEventListener("mouseover", function(){
+  this.classList.add("red")
+});
+
+dontTouch.addEventListener("mouseout", function(){
+  this.classList.remove("red")
+});
+
+
+
+dontTouch.onclick = function(){
+  h1.classList.toggle("toggler")
+  h1.textContent = "I Told You";
+  image.setAttribute('src', "https://cdnb.artstation.com/p/assets/images/images/002/828/955/large/sabbas-apterus-blood-red-and-cyan.jpg?1466184215")
+  image.classList.add("width")
+  
+}
+
+greetings.onclick = function(){
+  sayHello();
 }
 
 
@@ -64,11 +98,30 @@ function changeH1(){
 
   function changedImage(){
       let currentSrc = image.getAttribute('src');
-      if (currentSrc === "https://t4.ftcdn.net/jpg/02/66/76/83/240_F_266768305_jxxjP3ivAYLHxbOejYQ4095SvaGfTjc3.jpg"){
-          image.setAttribute ('src' , 'https://image.shutterstock.com/image-vector/initial-letter-js-logotype-company-260nw-713410882.jpg')
-      }else{
-        image.setAttribute ('src', 'https://t4.ftcdn.net/jpg/02/66/76/83/240_F_266768305_jxxjP3ivAYLHxbOejYQ4095SvaGfTjc3.jpg') 
+         if (currentSrc === "https://t4.ftcdn.net/jpg/02/66/76/83/240_F_266768305_jxxjP3ivAYLHxbOejYQ4095SvaGfTjc3.jpg"){
+             image.setAttribute ('src' , 'https://image.shutterstock.com/image-vector/initial-letter-js-logotype-company-260nw-713410882.jpg')
+          }else{
+             image.setAttribute ('src', 'https://t4.ftcdn.net/jpg/02/66/76/83/240_F_266768305_jxxjP3ivAYLHxbOejYQ4095SvaGfTjc3.jpg') 
       }
 
   }
+
+
+  function sayHello(){
+    let hello = prompt("Please, Enter you Name")
+    localStorage.setItem('name', hello)
+    h1.textContent = 'Guest Name, ' + hello;
+
+    if(!localStorage.getItem('name') ) {
+      sayHello();
+    }   if ( hello === null){
+           h1.textContent = "Why Dont you like me :("
+    }
+    
+      else {
+        let storedName = localStorage.getItem('name');
+        h1.textContent = 'Hello'+ ' ' + storedName + ',' + 'welcome ';
+    }
+  }
+
 
